4c7d802d24b0b72ff3358a2757c616d4f6d6fc89
John Jimenez <johnjimenez@Johns-MacBook-Pro.local>
2013-01-22 23:39:56 -0800

    Fix teleopring so is bound withint field of play.

--- app/assets/javascripts/movement.js
+++ app/assets/javascripts/movement.js
@@ -1,4 +1,6 @@
 var movement = function(speed, distance){
+  y = $('#draw-target').position().top + $('#draw-target').height() - 40;
+  x = $('#draw-target').width() - 40;
   return {
     kikuchiyo:{
       '104':{ x:-speed,    y:0, z:null}, // "h" => left 
@@ -16,9 +18,9 @@ var movement = function(speed, distance){
       '72': { x: -1, y:0, z:'teleport' },
       '48': { x:0 , y:-1, z:'teleport' },
 
-      '76': { x:-1,  y:window.innerHeight - 100, z:'teleport' },
-      '77': { x:-1,  y:window.innerHeight/2,     z:'teleport' },
-      '52': { x: window.innerWidth - 1, y:-1,  z:'teleport' }
+      '76': { x:-1,  y:y, z:'teleport' },
+      '77': { x:-1,  y:y/2,     z:'teleport' },
+      '52': { x: x, y:-1,  z:'teleport' }
     }
   };
 };
